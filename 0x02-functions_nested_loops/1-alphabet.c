76a4b1959018b2d60400976930672934d20f9cf9
Bilal Saou <bilal19saou@gmail.com>
2023-06-20 18:49:10 -0700

    solv the 1-alphabet.c task

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Description: prints _alphabet - utilizes on the _putchar finction to print
+ *                           the alphabet a - z
+ *
+*/
+
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
