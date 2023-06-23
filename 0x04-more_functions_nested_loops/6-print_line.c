a0c8e0e0e4c9f02e10df4e69d29a1681fc8f3c48
Bilal Saou <bilal19saou@gmail.com>
2023-06-22 20:19:46 -0700

    edit a 6-print_line.c file

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,7 +4,7 @@
  * print_line - print a straight line
  *
  * @n: is the number of times the _ character
- *     schould be printed
+ *     should be printed
 */
 
 void print_line(int n)
@@ -12,11 +12,15 @@ void print_line(int n)
 	int lnChr;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (lnChr = 1; lnChr <= n; lnChr++)
+		{
 			_putchar('_');
+		}
 		_putchar('\n');
 	}
 }
