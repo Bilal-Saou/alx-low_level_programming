38d6575682360196f1946bcd93b2a83377ca5f91
Bilal Saou <bilal19saou@gmail.com>
2023-06-26 18:34:48 -0700

    edit the 7-puts_half.c file

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,27 +8,23 @@
 
 void puts_half(char *str)
 {
-	int len, n, i;
+	int index, half;
 
-	len = 0;
-
-	while (str[len] != '\0')
+	index = 0;
+	while (str[index] != '\0')
 	{
-		len++;
+		index++;
 	}
 
-	if (len % 2 == 0)
+	half = index / 2;
+
+	if (index % 2 == 1)
+		half++;
+
+	while (half < index)
 	{
-		for (i = len / 2; str[i] != '\0'; i++)
-		{
-			putchar(str[i]);
-		}
-	} else if (len % 2)
-	{
-		for (n = (len - 1) / 2; n < len - 1; n++)
-		{
-			_putchar(str[n + 1]);
-		}
+		_putchar(str[half]);
+		half++;
 	}
 	_putchar('\n');
 }
